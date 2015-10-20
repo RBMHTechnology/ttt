2ea9c5347f2fb4c75f86bfacf8fb946ca26e77d7
Michal Samek <Michal.Samek@at.redbullmediahouse.com>
2015-10-20 09:32:44 +0200

    [tt2webvvt] Removal of unused Constants.

--- ttt-tt2webvtt/src/main/java/com/redbullmediabase/resourcelayer/processing/ttt/webvtt/parser/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.redbullmediabase.resourcelayer.processing.ttt.webvtt.parser;
-
-/**
- *
- * @author Michal Samek, (Michal.Samek at at.redbullmediahouse.com)
- */
-public class Constants {
-    
-}
