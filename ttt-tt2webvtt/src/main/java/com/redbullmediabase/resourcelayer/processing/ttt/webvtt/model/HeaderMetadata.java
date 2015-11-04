0dbbc6d546f217b6b3c5657d6d32745b6002439b
Michal Samek <Michal.Samek@at.redbullmediahouse.com>
2015-11-04 15:21:03 +0100

    Removal of tt2webvtt as it got integrated into processing-tty.

--- ttt-tt2webvtt/src/main/java/com/redbullmediabase/resourcelayer/processing/ttt/webvtt/model/HeaderMetadata.java
+++
@@ -1,9 +0,0 @@
-package com.redbullmediabase.resourcelayer.processing.ttt.webvtt.model;
-
-/**
- *
- * @author Michal Samek, (Michal.Samek at at.redbullmediahouse.com)
- */
-public interface HeaderMetadata {
-    
-}
